b48e9937d0954cd274244e221e39009e302a5320
Aaron Lal <lucidlear@gmail.com>
2024-08-11 09:04:24 +1000

    Add project files.

---
+++ StudentGrades/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var gradeCalculator = new GradeCalculator();
+
+            Console.WriteLine("Enter the percentage: ");
+            var percentage = Convert.ToInt32(Console.ReadLine());
+
+            var grade = gradeCalculator.GetGradeByPercentage(percentage);
+
+            Console.WriteLine("Student Grade: {0}", grade);
+        }
+    }
+}
---
+++ StudentGrades/TestGradeCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StudentGrades
+{
+    [TestFixture]
+    public class TestGradeCalculator
+    {
+        private GradeCalculator _gradeCalculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _gradeCalculator = new GradeCalculator();
+        }
+
+        [Test]
+        public void TestGetGradeByPercentage()
+        {
+            var percentage = 99;
+
+            var grade = _gradeCalculator.GetGradeByPercentage(percentage);
+
+            Assert.That("A", Is.EqualTo(grade));
+
+        }
+    }
+}
